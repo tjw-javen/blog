db503eb7fa46d9ab9a5efce5aab2d4f291f2fb7e
tanjiawen <tanjiawen@dm-ai.cn>
2021-11-16 19:54:04 +0800

    修改首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,35 @@
 module.exports = {
-    title: 'hello vuepress',
-    description: 'Just playing around',
+    title: 'tanjiawen',
+    description: 'Just do it',
     base: '/blog/',
+    head: [
+      ['link', { rel: 'icon', href: '/icon.png' }]
+    ],
     themeConfig: {
-    //   logo: '/logo.png',
+      logo: '/logo.jpg',
       nav: [
         { text: '首页', link: '/' },
         { text: '指南', link: '/guide/' },
+        {
+          text:'书籍',
+          items:[
+            {
+              text:'js',
+              items:[
+                {
+                  text:'基础',
+                  link:'/books/a/'
+                },
+                {
+                  text:'基础1',
+                  link:'/books/b/'
+                },
+              ]
+            }
+          ]
+        },
         { text: 'API', link: '/api/' },
-        { text: 'Github', link: 'https://github.com/yes1am/dry' },
+        { text: 'Github', link: 'https://github.com/tjw-javen/blog' },
       ],
       sidebar: {
         '/guide/': [
--- docs/README.md
+++ docs/README.md
@@ -1,24 +1,16 @@
-# hello word
-
-::: tip
-这是一个提示
-:::
-
-::: warning
-这是一个警告
-:::
-
-::: danger
-这是一个危险警告
-:::
-
-::: details
-这是一个详情块，在 IE / Edge 中不生效
-:::
-
-```js
-export default {
-  name: "MyComponent",
-  // ...
-};
-```
+---
+home: true
+lang: zh-CN
+heroText: 贵在坚持
+heroImage: /logo.jpg
+actionText: 开始 →
+actionLink: /guide/
+features:
+  - title: A Blog
+    details: 专注写作前端博客，记录日常所得。
+  - title: For Me
+    details: 故九万里，则风斯在下矣，而后乃今培风；背负青天，而莫之夭阏者，而后乃今将图南。
+  - title: For Future
+    details: 记录个人博客，见证成长之路。
+footer: Copyright © 2021-present Tanjiawen
+---
